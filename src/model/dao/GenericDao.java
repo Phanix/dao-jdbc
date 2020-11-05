326a3bb6a57c75f06d922f16beee2ed4156744ee
Phanix <fh2game86@gmail.com>
2020-11-05 20:50:51 -0300

    DepartmentDao and SellerDao Interfaces

---
+++ src/model/dao/GenericDao.java
@@ -0,0 +1,17 @@
+package model.dao;
+
+import java.util.List;
+
+public interface GenericDao<T> {
+	
+	void insert(T t);
+	
+	void update(T t);
+	
+	void deleteById(Integer id);
+	
+	T findById(Integer id);
+	
+	List<T> findAll();
+
+}
